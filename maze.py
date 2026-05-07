2ce50ffea5e123a1f4635e5d1f8ea8dfd0f529fc
Yohannis <yotadzin@gmail.com>
2026-05-07 03:24:08 +0300

    fixed

--- maze.py
+++ maze.py
@@ -37,7 +37,7 @@ class Game:
         pygame.display.set_caption("Maze")
         self.clock = pygame.time.Clock()
         self.running = True
-        self.maze = None
+        self.maze = Maze(rows, cols)
     
     def draw_cell(self, r, c, color=None):
         x = c * self.cell
@@ -52,39 +52,58 @@ class Game:
             pygame.draw.line(self.screen, BLACK, (x, y), (x, y + self.cell), 2)
         if r == self.rows - 1:
             pygame.draw.line(self.screen, BLACK, (x, y + self.cell), (x + self.cell, y + self.cell), 2)
-        def get_nb(self, row, col):
+    
+    def draw(self, path=None, vis=None, dead=None):
+        self.screen.fill(WHITE)
+        for i in range(self.rows):
+            for j in range(self.cols):
+                color = None
+                if (i, j) == self.maze.start:
+                    color = GREEN
+                elif (i, j) == self.maze.end:
+                    color = RED
+                elif dead and (i, j) in dead:
+                    color = BLUE
+                elif path and (i, j) in path and (i, j) not in [self.maze.start, self.maze.end]:
+                    color = RED
+                elif vis and vis[i][j] and (i, j) not in [self.maze.start, self.maze.end]:
+                    color = GRAY
+                self.draw_cell(i, j, color)
+        pygame.display.flip()
+    
+    def get_nb(self, row, col):
         n = []
-        if row > 0 and not self.vis[row-1][col]:
+        if row > 0 and not self.maze.vis[row-1][col]:
             n.append((row-1, col, 'n'))
-        if row < self.rows - 1 and not self.vis[row+1][col]:
+        if row < self.rows - 1 and not self.maze.vis[row+1][col]:
             n.append((row+1, col, 's'))
-        if col > 0 and not self.vis[row][col-1]:
+        if col > 0 and not self.maze.vis[row][col-1]:
             n.append((row, col-1, 'w'))
-        if col < self.cols - 1 and not self.vis[row][col+1]:
+        if col < self.cols - 1 and not self.maze.vis[row][col+1]:
             n.append((row, col+1, 'e'))
         return n
     
     def eat(self, r1, c1, r2, c2, d):
         if d == 'n':
-            self.north[r1][c1] = 0
+            self.maze.north[r1][c1] = 0
         elif d == 's':
-            self.north[r2][c2] = 0
+            self.maze.north[r2][c2] = 0
         elif d == 'w':
-            self.east[r1][c1-1] = 0
+            self.maze.east[r1][c1-1] = 0
         elif d == 'e':
-            self.east[r1][c1] = 0
+            self.maze.east[r1][c1] = 0
     
     def generate_stack(self, delay=None):
         for i in range(self.rows):
             for j in range(self.cols):
-                self.north[i][j] = 1
-                self.east[i][j] = 1
-                self.vis[i][j] = False
+                self.maze.north[i][j] = 1
+                self.maze.east[i][j] = 1
+                self.maze.vis[i][j] = False
         
         sr = random.randint(0, self.rows-1)
         sc = random.randint(0, self.cols-1)
         stack = [(sr, sc)]
-        self.vis[sr][sc] = True
+        self.maze.vis[sr][sc] = True
         
         if delay:
             if not delay():
@@ -96,7 +115,7 @@ class Game:
             if nb:
                 nr, nc, d = random.choice(nb)
                 self.eat(r, c, nr, nc, d)
-                self.vis[nr][nc] = True
+                self.maze.vis[nr][nc] = True
                 stack.append((nr, nc))
                 if delay:
                     if not delay():
@@ -111,80 +130,29 @@ class Game:
         self.reset_vis()
         return True
     
-    def generate_queue(self, delay=None):
-        for i in range(self.rows):
-            for j in range(self.cols):
-                self.north[i][j] = 1
-                self.east[i][j] = 1
-                self.vis[i][j] = False
-        
-        sr = random.randint(0, self.rows-1)
-        sc = random.randint(0, self.cols-1)
-        queue = deque()
-        queue.append((sr, sc))
-        self.vis[sr][sc] = True
-        
-        if delay:
-            if not delay():
-                return False
-        
-        while queue:
-            r, c = queue.popleft()
-            nb = self.get_nb(r, c)
-            if nb:
-                nr, nc, d = random.choice(nb)
-                self.eat(r, c, nr, nc, d)
-                self.vis[nr][nc] = True
-                queue.append((nr, nc))
-                if delay:
-                    if not delay():
-                        return False
-        
-        self.pick_start_end()
-        self.reset_vis()
-        return True
-    
     def pick_start_end(self):
-        self.start = (random.randint(0, self.rows-1), 0)
-        self.east[self.start[0]][0] = 0
-        self.end = (random.randint(0, self.rows-1), self.cols-1)
+        self.maze.start = (random.randint(0, self.rows-1), 0)
+        self.maze.east[self.maze.start[0]][0] = 0
+        self.maze.end = (random.randint(0, self.rows-1), self.cols-1)
     
     def reset_vis(self):
-        self.vis = [[False for _ in range(self.cols)] for _ in range(self.rows)]
-        def add_cycles_targeting_end(self):
-        for i in range(self.rows):
-            for j in range(self.cols):
-                if random.random() < 0.05:
-                    if i > 0 and self.north[i][j] == 1:
-                        self.north[i][j] = 0
-                if random.random() < 0.05:
-                    if j < self.cols - 1 and self.east[i][j] == 1:
-                        self.east[i][j] = 0
-        
-        end_r, end_c = self.end if self.end else (self.rows//2, self.cols-1)
-        for _ in range(3):
-            r = max(0, min(self.rows-1, end_r + random.randint(-2, 2)))
-            c = max(0, min(self.cols-1, end_c + random.randint(-2, 2)))
-            if r > 0 and self.north[r][c] == 1:
-                self.north[r][c] = 0
-            if c < self.cols - 1 and self.east[r][c] == 1:
-                self.east[r][c] = 0
+        self.maze.vis = [[False for _ in range(self.cols)] for _ in range(self.rows)]
     
     def solve_backtrack(self, delay=None):
-        stack = [(self.start[0], self.start[1], [])]
-        self.vis[self.start[0]][self.start[1]] = True
-        self.dead = []
+        stack = [(self.maze.start[0], self.maze.start[1], [])]
+        self.maze.vis[self.maze.start[0]][self.maze.start[1]] = True
+        self.maze.dead = []
         
         while stack:
             r, c, p = stack.pop()
             cur = p + [(r, c)]
             
             if delay:
-                if not delay(cur, self.vis, self.dead):
+                if not delay(cur, self.maze.vis, self.maze.dead):
                     return None
             
-            if (r, c) == self.end:
-                self.path = cur
+            if (r, c) == self.maze.end:
+                self.maze.path = cur
                 return cur
             
             dirs = ['n', 's', 'w', 'e']
@@ -193,124 +161,126 @@ class Game:
             for d in dirs:
                 nr, nc = r, c
                 ok = False
-                if d == 'n' and r > 0 and self.north[r][c] == 0:
+                if d == 'n' and r > 0 and self.maze.north[r][c] == 0:
                     nr, nc = r-1, c
                     ok = True
-                elif d == 's' and r < self.rows-1 and self.north[r+1][c] == 0:
+                elif d == 's' and r < self.rows-1 and self.maze.north[r+1][c] == 0:
                     nr, nc = r+1, c
                     ok = True
-                elif d == 'w' and c > 0 and self.east[r][c-1] == 0:
+                elif d == 'w' and c > 0 and self.maze.east[r][c-1] == 0:
                     nr, nc = r, c-1
                     ok = True
-                elif d == 'e' and c < self.cols-1 and self.east[r][c] == 0:
+                elif d == 'e' and c < self.cols-1 and self.maze.east[r][c] == 0:
                     nr, nc = r, c+1
                     ok = True
                 
-                if ok and not self.vis[nr][nc]:
-                    self.vis[nr][nc] = True
+                if ok and not self.maze.vis[nr][nc]:
+                    self.maze.vis[nr][nc] = True
                     stack.append((nr, nc, cur))
             
             has_unvisited = False
             for d in ['n', 's', 'w', 'e']:
                 nr, nc = r, c
                 ok = False
-                if d == 'n' and r > 0 and self.north[r][c] == 0:
+                if d == 'n' and r > 0 and self.maze.north[r][c] == 0:
                     nr, nc = r-1, c
                     ok = True
-                elif d == 's' and r < self.rows-1 and self.north[r+1][c] == 0:
+                elif d == 's' and r < self.rows-1 and self.maze.north[r+1][c] == 0:
                     nr, nc = r+1, c
                     ok = True
-                elif d == 'w' and c > 0 and self.east[r][c-1] == 0:
+                elif d == 'w' and c > 0 and self.maze.east[r][c-1] == 0:
                     nr, nc = r, c-1
                     ok = True
-                elif d == 'e' and c < self.cols-1 and self.east[r][c] == 0:
+                elif d == 'e' and c < self.cols-1 and self.maze.east[r][c] == 0:
                     nr, nc = r, c+1
                     ok = True
-                if ok and not self.vis[nr][nc]:
+                if ok and not self.maze.vis[nr][nc]:
                     has_unvisited = True
                     break
             
-            if not has_unvisited and (r, c) != self.end and (r, c) not in self.dead:
-                self.dead.append((r, c))
+            if not has_unvisited and (r, c) != self.maze.end and (r, c) not in self.maze.dead:
+                self.maze.dead.append((r, c))
                 if delay:
-                    delay(cur, self.vis, self.dead)
+                    delay(cur, self.maze.vis, self.maze.dead)
         
         return None
     
-    def solve_shoulder(self, delay=None):
-        r, c = self.start
-        facing = 'e'
-        path = [(r, c)]
-        visited = set()
-        visited.add((r, c))
-        stuck = 0
+    def animate_gen(self):
+        def step():
+            self.draw()
+            self.clock.tick(60)
+            for e in pygame.event.get():
+                if e.type == pygame.QUIT:
+                    return False
+                if e.type == pygame.KEYDOWN and e.key == pygame.K_ESCAPE:
+                    return False
+            return True
         
-        while (r, c) != self.end and stuck < 5000:
-            if delay:
-                if not delay(path, visited, []):
-                    return None
-            
-            order = ['left', 'forward', 'right', 'back']
-            
-            for turn in order:
-                if facing == 'n':
-                    if turn == 'left':
-                        dr, dc, new_facing = 0, -1, 'w'
-                    elif turn == 'forward':
-                        dr, dc, new_facing = -1, 0, 'n'
-                    elif turn == 'right':
-                        dr, dc, new_facing = 0, 1, 'e'
-                    else:
-                        dr, dc, new_facing = 1, 0, 's'
-                elif facing == 's':
-                    if turn == 'left':
-                        dr, dc, new_facing = 0, 1, 'e'
-                    elif turn == 'forward':
-                        dr, dc, new_facing = 1, 0, 's'
-                    elif turn == 'right':
-                        dr, dc, new_facing = 0, -1, 'w'
-                    else:
-                        dr, dc, new_facing = -1, 0, 'n'
-                elif facing == 'w':
-                    if turn == 'left':
-                        dr, dc, new_facing = 1, 0, 's'
-                    elif turn == 'forward':
-                        dr, dc, new_facing = 0, -1, 'w'
-                    elif turn == 'right':
-                        dr, dc, new_facing = -1, 0, 'n'
-                    else:
-                        dr, dc, new_facing = 0, 1, 'e'
-                else:
-                    if turn == 'left':
-                        dr, dc, new_facing = -1, 0, 'n'
-                    elif turn == 'forward':
-                        dr, dc, new_facing = 0, 1, 'e'
-                    elif turn == 'right':
-                        dr, dc, new_facing = 1, 0, 's'
-                    else:
-                        dr, dc, new_facing = 0, -1, 'w'
-                
-                nr, nc = r + dr, c + dc
-                
-                if 0 <= nr < self.rows and 0 <= nc < self.cols:
-                    blocked = False
-                    if new_facing == 'n' and self.north[nr][nc] == 1:
-                        blocked = True
-                    elif new_facing == 's' and self.north[r][c] == 1:
-                        blocked = True
-                    elif new_facing == 'w' and self.east[nr][nc-1] == 1:
-                        blocked = True
-                    elif new_facing == 'e' and self.east[r][c] == 1:
-                        blocked = True
-                    
-                    if not blocked:
-                        r, c, facing = nr, nc, new_facing
-                        if (r, c) not in path:
-                            path.append((r, c))
-                        stuck = 0
-                        break
-            else:
-                stuck += 1
+        self.generate_stack(step)
+        self.draw()
+    
+    def animate_solve(self):
+        self.maze.vis = [[False for _ in range(self.cols)] for _ in range(self.rows)]
+        sol = []
         
-        self.path = path
-        return path if (r, c) == self.end else None
+        def step(path, vis, dead):
+            nonlocal sol
+            sol = path
+            self.draw(path, vis, dead)
+            self.clock.tick(60)
+            for e in pygame.event.get():
+                if e.type == pygame.QUIT:
+                    return False
+                if e.type == pygame.KEYDOWN and e.key == pygame.K_ESCAPE:
+                    return False
+            return True
+        
+        self.solve_backtrack(step)
+        for _ in range(60):
+            self.draw(sol, None, None)
+            self.clock.tick(30)
+            for e in pygame.event.get():
+                if e.type == pygame.QUIT:
+                    return
+                if e.type == pygame.KEYDOWN and e.key == pygame.K_ESCAPE:
+                    return
+    
+    def wait_space(self):
+        waiting = True
+        while waiting and self.running:
+            for e in pygame.event.get():
+                if e.type == pygame.QUIT:
+                    self.running = False
+                    return False
+                if e.type == pygame.KEYDOWN:
+                    if e.key == pygame.K_SPACE:
+                        waiting = False
+                    if e.key == pygame.K_ESCAPE:
+                        self.running = False
+                        return False
+            self.clock.tick(30)
+        return True
+    
+    def run(self):
+        print("Press SPACE to generate maze")
+        if not self.wait_space():
+            return
+        self.animate_gen()
+        print("Press SPACE to solve maze")
+        if not self.wait_space():
+            return
+        self.animate_solve()
+        print("Done! Press ESC to quit")
+        while self.running:
+            for e in pygame.event.get():
+                if e.type == pygame.QUIT:
+                    self.running = False
+                if e.type == pygame.KEYDOWN and e.key == pygame.K_ESCAPE:
+                    self.running = False
+            self.clock.tick(30)
+        pygame.quit()
+        sys.exit()
+
+if __name__ == "__main__":
+    game = Game(21, 31)
+    game.run()
